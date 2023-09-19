0c62c57aeb102ca0dcf518ceeffc48e94647972c
jean0405 <jeanpierrea0405@gmail.com>
2023-09-19 14:08:34 -0500

    fix: :bug: connection error corrected
    fixed error in connection which pertained to login credentials

--- server/src/databases/connectDB.js
+++ server/src/databases/connectDB.js
@@ -2,19 +2,20 @@ import "dotenv/config";
 import { MongoClient } from "mongodb";
 
 let db = {
-  USER: process.env.USER,
-  PWD: process.env.DB_PWD
-}
+  USER: process.env.DB_USER,
+  PWD: process.env.DB_PWD,
+  DB_NAME: process.env.DB_NAME,
+};
 
-const URI = `mongodb+srv://${db.USER}:${db.PWD}@practica.4b4nkjj.mongodb.net/?retryWrites=true&w=majority`;
-export async function connDB(){
-    const client = new MongoClient(URI);
-    try {
-      await client.connect();
-      const db = client.db();
-      console.log("CONNECTED SUCCESS");
-      return db;
-    } catch (error) {
-      console.log("CONNECTION REFUSED", error.message);
-    }
+const URI = `mongodb+srv://${db.USER}:${db.PWD}@practica.4b4nkjj.mongodb.net/${db.DB_NAME}`;
+export async function connDB() {
+  const client = new MongoClient(URI);
+  try {
+    await client.connect();
+    const db = client.db();
+    console.log("CONNECTED SUCCESS");
+    return db;
+  } catch (error) {
+    console.log("CONNECTION REFUSED", error.message);
+  }
 }
