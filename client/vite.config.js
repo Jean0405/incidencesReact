973759bbd8c8dd8918c15a0e0005cc3151ec1e3c
jean0405 <jeanpierrea0405@gmail.com>
2023-09-19 09:19:55 -0500

    feat: :heavy_plus_sign: add dependences

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port: 5176,
+    host: "127.168.129.72"
+  }
 })
