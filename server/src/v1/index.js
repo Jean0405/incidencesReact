967281cbd05332d7f5afc4bc38a247403640354c
jean0405 <jeanpierrea0405@gmail.com>
2023-09-21 09:12:49 -0500

    feat: :sparkles: modules to edit report has been created
    this modules allows to trainer and support change fields in the report

--- server/src/v1/index.js
+++ server/src/v1/index.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import v1Reports from "./routes/reportRoutes.js";
+import v1Trainers from "./routes/trainerRoutes.js";
+import v1Supports from "./routes/supportRoutes.js";
 
 const v1Routes = Router();
 
-v1Routes.use("/reports", v1Reports);
+v1Routes.use("/reports", v1Reports)
+v1Routes.use("/trainers", v1Trainers)
+v1Routes.use("/supports", v1Supports)
 
 export default v1Routes;
