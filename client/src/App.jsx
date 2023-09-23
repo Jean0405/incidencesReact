6bd5ef4ec2ed4ab83de1bc94e2adb1518bfc7c35
jean0405 <jeanpierrea0405@gmail.com>
2023-09-23 05:58:26 -0500

    feat: :construction: campers component is on development

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CamperPage } from "./components/pages/CamperPage";
+import { SignIn } from "./components/pages/SignIn";
 
 export default function App() {
   return (
     <div>
       <Router>
         <Routes>
+          <Route path="/" element={<SignIn />} />
           <Route path="/camperPage" element={<CamperPage />} />
         </Routes>
       </Router>
--- client/src/components/pages/CamperPage.jsx
+++ client/src/components/pages/CamperPage.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export const CamperPage = () => {
+export const CamperPage = (props) => {
 
   const [ubication, setUbication] = useState("");
   const [title, setTitle] = useState("");
   const [category, setCategory] = useState("");
   const [description, setDescription] = useState("");
+
 
   let data = {
     title: title,
@@ -29,7 +32,19 @@ export const CamperPage = () => {
     })).json();
 
     if (response.status == 200) {
-      console.log("report has been created")
+      toast.success("All good!", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      setTitle("");
+      setDescription("");
+      setUbication()
     } else {
       console.log(response);
     }
@@ -38,7 +53,7 @@ export const CamperPage = () => {
 
 
   return (
-    <div>
+    <div className="h-screen">
       {/* NAVBAR */}
       <div className="navbar sm:w-full md:1/2">
         <div className="navbar-start">
@@ -46,55 +61,73 @@ export const CamperPage = () => {
         </div>
         <div className="navbar-end">
           <a className="navbar-item bg-primary font-bold px-4">+</a>
-          <a className="navbar-item">About</a>
-          <a className="navbar-item">Contact</a>
+          <a className="navbar-item">Sevedol</a>
+          <div className="avatar avatar-ring avatar-md">
+            <div className="dropdown-container">
+              <div className="dropdown">
+                <label className="btn btn-ghost flex cursor-pointer px-0" tabIndex="0">
+                  <div className="font-bold">S</div>
+                </label>
+                <div className="dropdown-menu dropdown-menu-bottom-left">
+                  <a className="dropdown-item text-sm">Log Out</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      {/* USER WELCOME */}
+      <div>
+        <h1 className="text-4xl font-bold text-center pt-3">Welcome <span className="text-sky-500">Camper</span></h1>
+      </div>
       {/* FORM */}
-      <section className="bg-gray-2 m-auto rounded-x max-w-4xl mt-5 rounded-md">
-        <div className="p-8 shadow-lg">
-          <form className="space-y-4"
-            onSubmit={createReport}>
-            <div className="w-full">
-              <input name="title" value={title} onChange={(e) => setTitle(e.target.value)} className="input input-solid max-w-full" placeholder="Title" type="text" />
-            </div>
-
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 max-w-full">
-              <div className="flex flex-col justify-center items-center">
-                <p className="font-bold text-sky-800">Ubication</p>
-                <select value={ubication} onChange={(e) => setUbication(e.target.value)} className="select select-solid-primary max-w-full mt-1">
-                  <option value="sputnik">Sputnik</option>
-                  <option value="apolo">Apolo</option>
-                  <option value="artemis">Artemis</option>
-                  <option value="review 1">Review 1</option>
-                  <option value="review 2">Review 2</option>
-                  <option value="hunters">Hunters</option>
-                </select>
+      <div className="mx-5">
+        <section className="bg-gray-2 m-auto rounded-x max-w-4xl mt-5 rounded-md">
+          <div className="p-8 shadow-lg">
+            <form className="space-y-4"
+              onSubmit={createReport}>
+              <div className="w-full">
+                <input name="title" value={title} onChange={(e) => setTitle(e.target.value)} className="input input-solid max-w-full" placeholder="Title" type="text" />
               </div>
-            </div>
-            <div>
-              <p className="text-center font-bold my-2 text-sky-800">Category</p>
-              <div id="group1" className="flex flex-row gap-3 justify-center items-center">
-                <div className="flex flex-row gap-2 items-center">
-                  <label htmlFor="radio1">Digital</label>
-                  <input name="category" value="digital" checked={category === "digital"} onChange={(e) => setCategory(e.target.value)} type="radio" className="radio radio-bordered-primary" />
-                </div>
-                <div className="flex flex-row gap-2 items-center">
-                  <label htmlFor="radio1">Physical</label>
-                  <input name="category" value="physical" checked={category === "physical"} onChange={(e) => setCategory(e.target.value)} type="radio" className="radio radio-bordered-primary" />
+
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 max-w-full">
+                <div className="flex flex-col justify-center items-center">
+                  <p className="font-bold text-sky-800">Ubication</p>
+                  <select value={ubication} onChange={(e) => setUbication(e.target.value)} className="select select-solid-primary max-w-full mt-1">
+                    <option value="sputnik">Sputnik</option>
+                    <option value="apolo">Apolo</option>
+                    <option value="artemis">Artemis</option>
+                    <option value="review 1">Review 1</option>
+                    <option value="review 2">Review 2</option>
+                    <option value="hunters">Hunters</option>
+                  </select>
                 </div>
               </div>
-            </div>
-            <div className="w-full">
-              <textarea name="description" value={description} onChange={(e) => setDescription(e.target.value)} className="textarea textarea-solid max-w-full" placeholder="Description" rows="8" id="message"></textarea>
-            </div>
+              <div>
+                <p className="text-center font-bold my-2 text-sky-800">Category</p>
+                <div id="group1" className="flex flex-row gap-3 justify-center items-center">
+                  <div className="flex flex-row gap-2 items-center">
+                    <label htmlFor="radio1">Digital</label>
+                    <input name="category" value="digital" checked={category === "digital"} onChange={(e) => setCategory(e.target.value)} type="radio" className="radio radio-bordered-primary" />
+                  </div>
+                  <div className="flex flex-row gap-2 items-center">
+                    <label htmlFor="radio1">Physical</label>
+                    <input name="category" value="physical" checked={category === "physical"} onChange={(e) => setCategory(e.target.value)} type="radio" className="radio radio-bordered-primary" />
+                  </div>
+                </div>
+              </div>
+              <div className="w-full">
+                <textarea name="description" value={description} onChange={(e) => setDescription(e.target.value)} className="textarea textarea-solid max-w-full" placeholder="Description" rows="8" id="message"></textarea>
+              </div>
 
-            <div className="mt-4">
-              <button type="submit" className="rounded-lg btn btn-primary btn-block">Create Report</button>
-            </div>
-          </form>
-        </div>
-      </section>
+              <div className="mt-4">
+                <button type="submit" className="rounded-lg btn btn-primary btn-block">Create Report</button>
+              </div>
+            </form>
+          </div>
+        </section>
+      </div>
+      <ToastContainer />
     </div>
   )
 }
