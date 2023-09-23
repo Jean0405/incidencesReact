66af06aa3d77d5cb451ddb0f24216b478be27528
jean0405 <jeanpierrea0405@gmail.com>
2023-09-23 00:28:04 -0500

    refactor: :recycle: campers router refactor

--- server/src/v1/index.js
+++ server/src/v1/index.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
+//controller imports
 import v1Reports from "./routes/reportRoutes.js";
+import v1Campers from "./routes/camperRoutes.js";
 import v1Trainers from "./routes/trainerRoutes.js";
 import v1Supports from "./routes/supportRoutes.js";
 import v1Auth from "./routes/authRoutes.js";
@@ -9,6 +11,7 @@ import { validateToken } from "../jwt/tokens.js";
 const v1Routes = Router();
 
 v1Routes.use("/reports", validateToken, v1Reports)
+v1Routes.use("/campers", validateToken, v1Campers)
 v1Routes.use("/trainers", validateToken, v1Trainers)
 v1Routes.use("/supports", validateToken, v1Supports)
 v1Routes.use("/auth", v1Auth);
