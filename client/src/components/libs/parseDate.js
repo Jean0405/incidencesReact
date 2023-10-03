d05e99fe368bdfe4ea654da5bc8ce28a7da7ab7c
jean0405 <jeanpierrea0405@gmail.com>
2023-10-03 09:13:35 -0500

    feat: :sparkles: this module allows parse the date
    will allow to display the creation dates of the reports

---
+++ client/src/components/libs/parseDate.js
@@ -0,0 +1,11 @@
+function parseDate(date) {
+  const newDate = new Date();
+
+  const year = newDate.getFullYear();
+  const month = (newDate.getMonth + 1).toString().padStart(2, "0");
+  const day = newDate.getDate().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
+
+export default parseDate;
